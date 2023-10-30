7c95594c31a80bb1bfb00b1b555a89d61d88f6e6
Stanislav Kabin <ioffline@make-it.app>
2023-10-30 16:08:06 +0300

    cosmetics

--- src/Http/Controllers/OrganizationsListController.php
+++ src/Http/Controllers/OrganizationsListController.php
@@ -4,8 +4,6 @@ namespace MakeIT\DiscreteApi\Organizations\Http\Controllers;
 
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 class OrganizationsListController extends BaseDiscreteApiOrganizationsController
 {
